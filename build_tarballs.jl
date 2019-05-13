45327c2c7ea54e2e55a2152cdba816b301a7810c
Isuru Fernando <isuruf@gmail.com>
2019-05-13 10:34:42 -0500

    Try gcc7

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -25,6 +25,7 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+platforms = BinaryBuilder.replace_gcc_version.(platforms, :gcc7)
 
 # The products that we will ensure are always built
 products(prefix) = [
